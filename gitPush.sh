8fdbaa48b82da6cc9262e23672cb1dbccfa2bee8
xiangang.zhu <xiangang.zhu@tcl.com>
2017-02-22 14:41:02 +0800

    add the script of commit the codes to github

---
+++ gitPush.sh
@@ -0,0 +1,7 @@
+#do some change
+#git init (if no ./git file)
+#git add *modifyed files*
+#git commit -m "the description of this commit"
+git remote rm origin
+remote add origin https://github.com/laozhuxinlu/GoogleMapWebApi.git
+git push origin master
