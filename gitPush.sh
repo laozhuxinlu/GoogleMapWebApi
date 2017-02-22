a66ad609833fd73498efaf1540d089e064c59429
xiangang.zhu <xiangang.zhu@tcl.com>
2017-02-22 14:44:43 +0800

    modify a error of gitPush.sh

--- gitPush.sh
+++ gitPush.sh
@@ -2,6 +2,7 @@
 #git init (if no ./git file)
 #git add *modifyed files*
 #git commit -m "the description of this commit"
+#*************************************************************
 git remote rm origin
-remote add origin https://github.com/laozhuxinlu/GoogleMapWebApi.git
+git remote add origin https://github.com/laozhuxinlu/GoogleMapWebApi.git
 git push origin master
